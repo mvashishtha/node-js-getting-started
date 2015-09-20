c625c1eb940d70e6da6a0f25e51a64e84a1869f9
MAHESH VASHISHTHA <vashishtha.mahesh@gmail.com>
2015-09-19 17:29:52 -0700

    d

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });*/
 
-//var cool = require('cool-ascii-faces');
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 

a217ac3185571a000000b9c5c84c76c7d63d24f7
MAHESH VASHISHTHA <vashishtha.mahesh@gmail.com>
2015-09-19 17:28:12 -0700

    d

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-      for (i=0; i < times; i++)
-          result += cool();
+  var result = '';
+  var times = process.env.TIMES || 5;
+  for (i=0; i < times; i++)
+    result += cool();
   response.send(result);
-    });
+});
 
 app.get('/cool', function(request, response) {                                                       
         response.send(cool());                                                                             
